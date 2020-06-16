f8ecefc1027ab8d7c850f88d3b4107e70d5b0b99
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-06-16 21:40:36 +0530

    Adding redux components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configStore from './store/configStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configStore();
+
+store.dispatch(addExpense({ description: 'water bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'gas bill', amount: 1000, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'rent', amount: 109500 }));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => (
   <div>
     This is from my edit expense component
+    {props.match.params.id}
   </div>
 );
 
