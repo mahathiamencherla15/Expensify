180695909b1c5da6951e9a0cc13a84d637f6005a
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-06-27 23:29:10 +0530

    Setting up env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCNSelqDlZqcZl-bMqX6CJxE5iS4RxtQWg",
-    authDomain: "expensify-648f3.firebaseapp.com",
-    databaseURL: "https://expensify-648f3.firebaseio.com",
-    projectId: "expensify-648f3",
-    storageBucket: "expensify-648f3.appspot.com",
-    messagingSenderId: "907270001250",
-    appId: "1:907270001250:web:a31fd9d80c03594583b73b",
-    measurementId: "G-B4QFPWV05G"
+    apiKey: process.env.FIREBASE_apiKey,
+    authDomain: process.env.FIREBASE_authDomain,
+    databaseURL: process.env.FIREBASE_databaseURL,
+    projectId: process.env.FIREBASE_projectId,
+    storageBucket: process.env.FIREBASE_storageBucket,
+    messagingSenderId: process.env.FIREBASE_messagingSenderId,
+    appId: process.env.FIREBASE_appId,
+    measurementId: process.env.FIREBASE_measurementId
   };
 
 firebase.initializeApp(firebaseConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if(process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development'});
+}
 
 module.exports = (env) =>{
   const isProduction = env === 'production';
@@ -10,7 +17,17 @@ module.exports = (env) =>{
     filename: 'bundle.js'
   },
   plugins: [
-    new MiniCssExtractPlugin('styles.css')
+    new MiniCssExtractPlugin('styles.css'),
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_apiKey': JSON.stringify(process.env.FIREBASE_apiKey),
+      'process.env.FIREBASE_authDomain': JSON.stringify(process.env.FIREBASE_authDomain),
+      'process.env.FIREBASE_databaseURL': JSON.stringify(process.env.databaseURL),
+      'process.env.FIREBASE_projectId': JSON.stringify(process.env.FIREBASE_projectId),
+      'process.env.FIREBASE_storageBucket': JSON.stringify(process.env.FIREBASE_storageBucket),
+      'process.env.FIREBASE_messagingSenderId': JSON.stringify(process.env.FIREBASE_messagingSenderId),
+      'process.env.FIREBASE_measurementId': JSON.stringify(process.env.FIREBASE_measurementId),
+      'process.env.FIREBASE_appId': JSON.stringify(process.env.FIREBASE_appId)
+    })
   ],
   module: {
     rules: [{
