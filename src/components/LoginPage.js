695346b07274fc6b5c72354c34b6e9e47d1a61f9
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-06-28 22:31:44 +0530

    Styling the login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,18 @@ import { startLogin } from '../actions/auth'
 
 
 const LoginPage = ({startLogin}) =>  (
-        <div>
-            <button onClick={startLogin}>Login</button>
+        <div className="box-layout">
+            <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify - A Budget App</h1>
+            <p>Get your expenses under control!</p>
+            <div class="google-btn">
+            <div class="google-icon-wrapper">
+                <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+            </div>
+            <p class="btn-text"><b onClick={startLogin}>Login with Google</b></p>
+            </div>
+            {/* <button className="box-layout__button">Login with Google</button> */}
+            </div>
         </div>
     )
 
