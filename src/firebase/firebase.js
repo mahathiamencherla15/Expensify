72620094f0e770f8b7b2e1e7d800fb6780bd941e
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-06-28 15:18:01 +0530

    Integrated firebase auth

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,4 +17,6 @@ firebase.analytics();
 
 const database = firebase.database();
 
-export {firebase, database as default};
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export {firebase, googleAuthProvider, database as default};
